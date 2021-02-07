12d3f1f20b9a73b58e87f6e5ebfcf1bd52eada89
Dagwood <sudhagarnarayanan@gmail.com>
2021-02-07 19:24:24 +0530

    initial commit

--- HerokuDeploy/settings.py
+++ HerokuDeploy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'j*e6+dbhlkd(f^zkyj(4qmfw@4y1ixe6mb#cx&x5f-+ympne)4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
